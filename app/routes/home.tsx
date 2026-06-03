af1bfc893653b7242da4103393598785ed73d1d3
Nate Kite <nrk67593@uga.edu>
2026-06-03 19:00:44 +0100

    Cleaned up gradient slightly

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -13,12 +13,12 @@ export default function Home() {
   // TODO: Refactor this gradient to clean it up
   return (
     <div className="parallax">
-      <div className="layer layer-1">
+      <div className="layer layer-1000">
         <div style={{height: "800px", overflow: "visible", display: "flex", alignItems: "flex-start", justifyContent: "center"}}>
-          <div style={{height: "2400px", width: "200vw", background: "linear-gradient(180deg, hsla(206, 93%, 57%, 1) 4%, hsla(196, 100%, 71%, 1) 39%, hsla(221, 68%, 52%, 1) 64%, hsla(224, 71%, 4%, 1) 87%)", padding: "100px", flexShrink: 0}}></div>
+          <div style={{height: "2400px", width: "200vw", background: "radial-gradient(90% 80% at 0% 0%, #00E8FFFF 0%, #073AFF00 100%),radial-gradient(40% 60% at 100% 0%, #25BF64FF 0%, #073AFF00 100%),radial-gradient(50% 50% at 60% 30%, #91CE91C4 0%, #073AFF00 100%),radial-gradient(50% 50% at 100% 30%, #00FFE4C2 0%, #00FF8B00 100%),radial-gradient(40% 40% at 10% 30%, #00FF8BB0 0%, #21DD7000 100%)", padding: "100px", flexShrink: 0}}></div>
         </div>
       </div>
-      <div className="layer layer-0">
+      <div className="layer layer-foreground">
         <div className="container mx-auto py-12 px-4">
           <h1 className="text-3xl font-heading font-bold mb-4">Nate Kite</h1>
           <div className="space-y-6 text-secondary-foreground">
